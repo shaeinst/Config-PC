7adb69092d2ce6869944319594daba7f9df343d8
Ali Shahid <shaeinst@gmail.com>
2022-08-11 21:32:37 +0545

    [setup.sh] add support for keyd

--- setup.sh
+++ setup.sh
@@ -6,3 +6,5 @@ cp -rf alacritty dunst rofi bspwm polybar sxhkd zsh ~/.config/
 echo "setting-up zsh"
 ln ~/.config/zsh/.zshrc ~/.zshrc
 
+echo "coping keyd config to /etc/keyd"
+sudo cp ./keyd/default.conf /etc/keyd/
