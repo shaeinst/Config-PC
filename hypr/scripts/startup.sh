c2f1bd87ccfdcf8b55c675d2dbc645518decd9e3
Ali Shahid <shaeinst@gmail.com>
2023-01-16 21:41:36 +0545

    [new config] add config for hyprland / hypr

---
+++ hypr/scripts/startup.sh
@@ -0,0 +1,12 @@
+
+# ## to fix java applications
+# # wmname LG3D
+sleep 2
+# to fix Android Studio
+export _JAVA_AWT_WM_NONREPARENTING=1
+
+# change default tmp dir
+export TMPDIR="$HOME/storeDrive/storeDriveBig/Temp"
+
+# for Notifications
+dunst &
