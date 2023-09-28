718e0411f8e3bf3ac0293e00e15c750bedf23ee4
Ali Shahid <shaeinst@gmail.com>
2023-09-29 00:57:57 +0545

    [hypr] some rules and improvements

--- hypr/scripts/hacks.sh
+++ hypr/scripts/hacks.sh
@@ -6,6 +6,7 @@ killall xdg-desktop-portal-hyprland
 killall xdg-desktop-portal-wlr
 killall xdg-desktop-portal
 /usr/lib/xdg-desktop-portal-hyprland &
-sleep 4
-/usr/lib/xdg-desktop-portal &
 
+sleep 4
+
+/usr/lib/xdg-desktop-portal &
